5c341a962f669bb3666e8c1f2b06cd68bc737115
jaydip <pjaydip956@gmail.com>
2024-04-07 18:39:11 +0530

    Commit

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,48 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 function Home() {
   return (
-    <div class="m-2 grid grid-flow-row gap-4 font-serif drop-shadow-md hover:drop-shadow-xl sm:grid-cols-3">
-      <div class="min-h-[250px] rounded-lg bg-orange-400 drop-shadow-md hover:bg-orange-500 hover:drop-shadow-xl">
-        <h1 class="m-20 flex justify-center text-5xl text-white">
-          Hello there👋
-        </h1>
+    <div class="m-8 grid grid-flow-row gap-8 font-serif drop-shadow-md hover:drop-shadow-xl sm:grid-cols-4">
+      <div class="max-h-[600px] w-auto rounded-lg bg-gray-800 drop-shadow-md hover:bg-orange-500 hover:drop-shadow-xl sm:row-span-2">
+        <img
+          src="./images/logoside.png"
+          alt="logoside"
+          class="object-cover  rounded-lg  "
+        />
       </div>
-      <div class="min-h-[250px] rounded-lg drop-shadow-md hover:drop-shadow-xl sm:col-span-2">
-        <img src="./images/1.png" class="object-cover h-64 w-full rounded-lg "/> 
 
+      <div class=" h-[320px]  w-auto rounded-lg drop-shadow-md hover:drop-shadow-xl sm:col-span-2 bg-slate-500">
+        <img
+          src="./images/1.png"
+          alt="poster"
+          class="object-cover h-[320px] w-[800px] rounded-lg"
+        />
       </div>
-      <div class="min-h-[50px] rounded-lg bg-red-400 drop-shadow-md hover:drop-shadow-xl sm:col-start-1 sm:row-span-2 sm:row-start-2"></div>
+      <div class="min-h-[300px] rounded-lg bg-red-400 drop-shadow-md hover:drop-shadow-xl text-black justify-center hover:text-white duration-500">
+        <h1 class="mx-8  mt-14 flex  text-4xl font-semibold ">Hello there👋</h1>
+
+        <h3 class="mx-8  mt-5 flex  text-xl ">
+          I am highly motivated B.tech IT student with deep intrest in Web
+          Technology and Cybeersecurity.I am highly motivated B.tech IT student
+          with deep intrest in Web Technology and Cybeersecurity.
+        </h3>
+      </div>
+
       <div class="min-h-[250px] rounded-lg bg-yellow-400 drop-shadow-md hover:drop-shadow-xl"></div>
-      <div class="min-h-[250px] rounded-lg bg-gray-400 drop-shadow-md hover:drop-shadow-xl ">
+
+      <div class="min-h-[250px] rounded-lg bg-gray-400 drop-shadow-md hover:drop-shadow-xl  sm:col-span-2 ">
         <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
           " Website without visitors is like a ship lost in the horizon. "
         </h2>
       </div>
-      <div class="min-h-[500px] rounded-lg bg-lime-500 drop-shadow-md hover:drop-shadow-xl sm:col-span-2 sm:col-start-2 sm:row-span-2 sm:row-start-3"></div>
-      <div class="min-h-[250px] rounded-lg bg-pink-400 drop-shadow-md hover:drop-shadow-xl"></div>
+
+      <div class="min-h-[500px] rounded-lg bg-lime-500 drop-shadow-md hover:drop-shadow-xl sm:col-span-2 sm:col-start-3 sm:row-span-2 sm:row-start-2"></div>
+
+      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl sm:col-span-4 sm:col-start-1 sm:row-span-1 sm:row-start-4 ">
+        <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
+          " Website without visitors is like a ship lost in the horizon. "
+        </h2>
+      </div>
     </div>
   );
 }
