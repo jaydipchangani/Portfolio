dfb0da351cf984818db48e5086b184c71f1f0bbc
jaydip <pjaydip956@gmail.com>
2024-07-03 18:15:59 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import Social from "./Social";
 import Resume from "./Resume";
 import backgroundImage from './image.png';
+import Silder from './Silder'
+
 
 function Home() {
   return (
@@ -49,13 +51,60 @@ function Home() {
         <Resume/>
       </div>
 
-      <div class="min-h-[500px] rounded-lg bg-lime-500 drop-shadow-md hover:drop-shadow-xl sm:col-span-2 sm:col-start-3 sm:row-span-2 sm:row-start-2"></div>
+      <div class="min-h-[500px] rounded-lg bg-white drop-shadow-md hover:drop-shadow-xl sm:col-span-2 sm:col-start-3 sm:row-span-2 sm:row-start-2">
 
-      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl sm:col-span-4 sm:col-start-1 sm:row-span-1 sm:row-start-4 ">
-        <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
-          " Website without visitors is like a ship lost in the horizon. "
+        <Silder/>
+      </div>
+
+      <div class="min-[h]-[500px] bg-white drop-shadow-md hover:drop-shadow-xl sm:col-span-4 sm:col-start-1 sm:row-span-1 sm:row-start-4  rounded-lg">
+        <h2 class=" text-center  text-3xl p-5 hover:text-4xl duration-700 ">
+        
+        Secure Code, Stunning Design: Building Websites You Can Trust
         </h2>
       </div>
+
+      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl  ">
+      <div
+      className="w-full p-6 flex flex-col  hover:bg-transparent transition duration-300 ease-in-out"
+      onMouseEnter={() => {
+        document.querySelector('.card-image').style.opacity = 1;
+      }}
+      onMouseLeave={() => {
+        document.querySelector('.card-image').style.opacity = 0;
+      }}
+    >
+      <div className="bg-white rounded shadow-md p-4 ">
+        <h5 className="text-lg font-bold mb-2">Web Development </h5>
+        <p className="text-gray-600">Building the bridge between your idea and the digital world</p>
+      </div>
+      <img
+        src="https://picsum.photos/200/300"
+        alt="Card image"
+        className="card-image absolute top-0 left-0 w-full h-full object-cover opacity-0 transition duration-300 ease-in-out"
+      />
+    </div>
+
+
+        
+      </div>
+
+      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl  ">
+        <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
+           Cybersecurity          </h2>
+      </div>
+
+      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl  ">
+        <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
+          Content Writing
+        </h2>
+      </div>
+
+      <div class="min-[h]-[500px] bg-blue-500 drop-shadow-md hover:drop-shadow-xl  ">
+        <h2 class=" flex text-center mt-16 text-4xl text-white hover:text-black  hover:font-light">
+          Website Design
+        </h2>
+      </div>
+
     </div>
   );
 }
---
+++ src/Silder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Carousel } from "@material-tailwind/react";
+
+function Silder()
+{
+    return(
+        
+        <div>
+            Hello
+        </div>
+        )
+    
+}
+
+export default Silder;
